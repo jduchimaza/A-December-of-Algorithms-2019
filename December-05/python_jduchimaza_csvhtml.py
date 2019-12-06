7a1b66097106c3ac8b75bbeba8484d9da8c33afd
Juan J Duchimaza <jduchimaza@gmail.com>
2019-12-05 21:21:59 -0500

    initial commit

---
+++ December-05/python_jduchimaza_csvhtml.py
@@ -0,0 +1,28 @@
+# csv2html 
+# Brief: Function to convert a CSV file to HTML table
+# Author: Juan D.H.
+# Date:   5 Dec 2019
+# 
+# This function was written as part of 'A December of Algorithms'
+#----------------------------------------------------------------
+
+import csv
+
+def csv2html(csvfile):
+   with open(csvfile,'r') as csvf:
+      csvr = csv.reader(csvf,skipinitialspace=True)
+      html = open('html_res.html','w+')
+      html.write('<html>\n\t<body>\n\t\t<table>\n') #Write the html tags needed
+      for row in csvr:
+         html.write('\t\t\t<tr>') #Write tags for table row
+         for col in row:
+            html.write('<th>'+str(col)+'</th>') # Add a cell to the row
+         html.write('</tr>\n') #Write tags to close table row
+      html.write('\t\t</table>\n\t</body>\n</html>') #Write the closing tags
+      html.close()
+      csvf.close()
+   return 
+
+#Testcase
+filename = 'csv_to_html_res.csv'
+csv2html(filename)
